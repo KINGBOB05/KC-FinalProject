7c329e9115794ad6e65b1072d94d0d3e3d74af5c
KINGBOB05 <kuwaitigamer777@gmail.com>
2022-09-04 22:24:08 +0300

    i think im done + finished js i think

---
+++ fp.js
@@ -0,0 +1,10 @@
+document.querySelector("#redirect")
+     .addEventListener("click", () => {
+          window.location.href = "./rl.html";
+        });
+
+
+document.querySelector("#redirect2")
+     .addEventListener("click", () => {
+         window.location.href = "./over.html";
+        });
---
+++ ow.js
@@ -0,0 +1,15 @@
+document.querySelector("#redirect")
+     .addEventListener("click", () => {
+          window.location.href = "https://www.youtube.com/c/CyxOW";
+        });
+
+document.querySelector("#redirect2")
+     .addEventListener("click", () => {
+          window.location.href = "https://www.youtube.com/c/YZNSA";
+        });
+
+
+document.querySelector("#redirect3")
+     .addEventListener("click", () => {
+         window.location.href = "https://www.youtube.com/c/mL7support";
+        });
